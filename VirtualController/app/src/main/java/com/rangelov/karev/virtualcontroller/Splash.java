643704f4e78f45f2d77580e5d0ab67d4e53a80d1
Nikolay Rangelov <nikrangelov@gmail.com>
2016-03-29 10:31:07 +0300

    Initila commit

---
+++ VirtualController/app/src/main/java/com/rangelov/karev/virtualcontroller/Splash.java
@@ -0,0 +1,15 @@
+package com.rangelov.karev.virtualcontroller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by root on 3/25/16.
+ * https://www.youtube.com/watch?v=XwOuTjUFexE
+ */
+public class Splash extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
